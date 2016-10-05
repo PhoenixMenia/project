cab881f78ba3aea6a6f6c6645c55b68b4b138892
Phoenix <764491014@qq.com>
2016-10-05 18:15:42 +0800

    addsald

--- js/prod_details.js
+++ js/prod_details.js
@@ -24,4 +24,42 @@ $(function(){
 		$(this).hide();
 		$('.item_list').hide();
 	});
+
+	/*点击或者鼠标悬浮时分享的方框显示*/
+	$('#shareto').hover(function(){
+		$(this).parent('.share').next('.share1').show();
+	},function(){
+		$(this).parents('.share').next('.share1').hide();
+	}).on('click',function(){
+		var html = $('#share_more').html();
+		$('#share_more').get(0).innerHTML += $('.share1').get(0).innerHTML;
+		$('#share_more').show().find('#close').on('click',function(){		
+			$(this).parents('#share_more').html(html).hide();
+		});
+	});
+
+	$('.share1').hover(function(){
+		$(this).show().find('#more').click(function(){
+			var html =  $('#share_more').html();
+			$('#share_more').get(0).innerHTML += $('.share1').get(0).innerHTML;
+			$('#share_more').show().find('#close').on('click',function(){		
+				$(this).parents('#share_more').html(html).hide();
+			});
+		});
+	},function(){
+		$(this).hide();
+	});
+
+	//选项卡切换；
+	$('.list li').each(function(index,element){
+		$(this).on('click',function(){
+			$(this).addClass('current').siblings('li').removeClass('current');
+			$('.list_det div').eq(index).show().siblings('div').hide();
+		});
+	});
+
+
+
+
+
 });
