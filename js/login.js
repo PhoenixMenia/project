3a36b906bd0a0a146430d73e075e4a918c5c0678
Phoenix <764491014@qq.com>
2016-10-07 14:43:53 +0800

    change login

--- js/login.js
+++ js/login.js
@@ -13,7 +13,58 @@ $(function(){
 		$('.verification span').text(veri_str);
 	}
 
-	$(window).on('load',verification);
+	$(window).on('load',function(){
+		verification();
+		$.cookie.json = true;
+		var user = $.cookie('userInfo');
+		if(user){
+			$('#username').val(user.username);
+			$('#password').val(user.password);
+		}
+	});
 	$('.verification a').on('click',verification);
+
+	/*--------------------记住用户名和密码-------------------*/
+	$('.login_btn').on('click',function(){
+		$.cookie.json = true;
+		var status = $('#remenber').prop('checked'),
+			username = $('#username').val(),
+			password = $('#password').val();
+			userInfo = {
+				'username':username,
+				'password':password
+			};
+		if(status){	
+			$.cookie('userInfo',userInfo,{'expires':7});
+		}else{
+			$.cookie('userInfo',userInfo,{'expires':-1});
+		}
+
+		/*-------判断验证码是否正确------*/
+		if(!isTrue){
+			return;
+		}else{
+			console.log('验证码正确');
+			//待完善。。。。。提交登录
+		}
+	
+	});
+
+	/*-----------------判断验证码是否正确--------------*/
+	
+	$('#veristring').on('blur',isVerificationTrue);
+	var isTrue;
+	function isVerificationTrue(){
+		if($(this).val().toUpperCase() == $('#confr').text().toUpperCase()){
+			$(this).siblings('i').text('验证码正确!');
+			isTrue = true;
+		}else{
+			$(this).siblings('i').text('验证码错误!');
+			isTrue = false;
+		}
+	}
+
+
+
 		
 });
